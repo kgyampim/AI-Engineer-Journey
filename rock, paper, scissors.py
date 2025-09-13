ad8a86f7a2d52a91f02bcb86ca3b7886c22ab998
Kweku Gyampim <gyampimkweku@gmail.com>
2025-09-13 23:25:56 +0000

    variables,loops, conditional flows, and functions in this simple rock, paper, scissors game. Set to build upon.

---
+++ rock, paper, scissors.py
@@ -0,0 +1,22 @@
+import random
+
+options = ['rock', 'paper', 'scissors']
+
+while True:
+    user = input('rock, paper, or scissors? (q to quit): ').lower()
+    if user == 'q':
+        break
+comp  = random.choice(options)
+print(f'computer chose: {comp}')
+if user == comp:
+    print('game ends in a tie, since all players chose the same options!')
+elif (user == 'rock' and comp == 'scissors') or \
+     (user == 'paper' and comp == 'rock') or \
+     (user == 'scissors' and comp == 'paper'):
+    print('You win!')
+else:
+    print('You lose!')
+
+print("Game Over!")
+    
+
