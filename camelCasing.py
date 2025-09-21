9d53744f31f6bf5cd3cc53b345e659311dbfd164
Kweku Gyampim <gyampimkweku@gmail.com>
2025-09-21 03:54:49 +0000

    Improvements, bugs fix, better user experience.

--- camelCasing.py
+++ camelCasing.py
@@ -6,7 +6,7 @@ compScores = 0
 rounds = 0
 results = []
 
-while rounds < 7 and userScores < 3 and compScores < 3:
+while rounds < 7 and userScores < 4 and compScores < 4:
     print(f'---Round {rounds +1}---')
     user = input('rock, paper, or scissors? (q to quit): ').lower()
     if user == 'q':
@@ -14,7 +14,8 @@ while rounds < 7 and userScores < 3 and compScores < 3:
     comp = random.choice(options)
     print(f'computer chose: {comp}')
     if user == comp:
-        outcome = "A Tie!"
+        print("It's a tie, No round counted.\n")
+        continue 
     elif (user == 'rock' and comp == 'scissors') or \
          (user == 'paper' and comp== 'rock') or \
          (user == 'scissors' and comp == 'paper'):
@@ -26,19 +27,19 @@ while rounds < 7 and userScores < 3 and compScores < 3:
 
     rounds += 1
     results.append((rounds, user, comp, outcome))
-    print(f'Round {rounds} Results -> {outcome}')
+    print(f'Round {rounds} Winner -> {outcome}')
     print(f'Score -> You: {userScores}, Computer: {compScores}')
 
 print("Game Review")
 for r in results:
    print(f'Round {r[0]} \n You chose {r[1]} -> Computer chose {r[2]} \n Winner: {r[3]}')
 print('---Final results---')
-if userScores == 3:
+if userScores == 4:
    print(f'You are the overall winner!')
-elif compScores == 3:
+elif compScores == 4:
    print(f'Computer is the overall winner!')
 else:
-   print("Game Over! No one reached 3 wins in 7 rounds.")
+   print("Game Over! No one reached 4 wins in 7 rounds.")
 
     
 
